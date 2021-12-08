e06b706dae25ce5da98e5ebf21cb34b5344fefd9
Stan Fortoński <contact@stanfortonski.dev>
2021-12-08 19:10:48 +0100

    Test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('<App />', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+    const linkElement = screen.getByText(/React Weather App/i);
     expect(linkElement).toBeInTheDocument();
 });
